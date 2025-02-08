0ffa12c6f542be265035be1451b329c33462fbd4
CJR-25-ai <legalzinho999@gmail.com>
2025-02-08 14:21:06 -0300

    Add files via upload

---
+++ Models/Order.cs
@@ -0,0 +1,13 @@
+
+namespace Televendas
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
+    
